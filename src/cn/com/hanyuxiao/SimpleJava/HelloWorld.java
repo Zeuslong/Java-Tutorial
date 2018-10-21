08e741365d973cdcc2b663fbd89fc71ba30cf4cf
hanyuxiao <hanorz@foxmail.com>
2018-10-21 09:23:38 +0800

    feat：经典的 hello world 程序

---
+++ src/cn/com/hanyuxiao/SimpleJava/HelloWorld.java
@@ -0,0 +1,13 @@
+package cn.com.hanyuxiao.SimpleJava;
+
+/**
+ * 经典代码 hello world
+ *
+ * @author hanyuxiao
+ * @version 1.8
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
