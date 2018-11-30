2542fca38432bc4bbcfcdc78bbd3164639d06980
hanyuxiao <hanyuxiao@baidu.com>
2018-11-30 10:35:14 +0800

    feat：ExecutorService 的一个简单的 submit 使用

---
+++ src/cn/com/hanyuxiao/SimpleJava/java/util/concurrent/ExecutorServiceExample.java
@@ -0,0 +1,38 @@
+package cn.com.hanyuxiao.SimpleJava.java.util.concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * {@link java.util.concurrent.ExecutorService} 的简单使用，由于它是一个接口，所以
+ * 这里具体的线程池使用 {@link Executors#newCachedThreadPool()}
+ *
+ * @see ExecutorsExample
+ * @author hanyuxiao
+ * @since 1.8
+ */
+public class ExecutorServiceExample {
+    public static void main(String[] args) {
+        ExecutorServiceExample executorServiceExample = new ExecutorServiceExample();
+        executorServiceExample.submit();
+    }
+
+    /**
+     * {@link ExecutorService#submit(Runnable)} 的一个简单的使用示例，在这个示例当中，创建了一个
+     * 线程池，然后往线程池当中提交了 10 个任务让线程池执行
+     */
+    public void submit() {
+        class Task implements Runnable {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+            }
+        }
+
+        ExecutorService pool = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < 10; i ++) {
+            pool.submit(new Task());
+        }
+    }
+}
