07e859ab4a6d896ac81ced27dcc74ca90393925f
hanyuxiao <hanyuxiao@baidu.com>
2018-12-05 14:23:48 +0800

    feat：Runnable 的另类写法

--- src/cn/com/hanyuxiao/SimpleJava/java/lang/RunnableExample.java
+++ src/cn/com/hanyuxiao/SimpleJava/java/lang/RunnableExample.java
@@ -34,4 +34,23 @@ public class RunnableExample {
         Thread thread = new Thread(runnable);
         thread.start();
     }
+
+    /**
+     * 这种使用 {@link java.lang.Runnable} 的方法是一种简便的写法，但是
+     * 不是很适合在复杂的环境当中使用，适合于在一次性任务当中使用。
+     */
+    public void runNoImplements() {
+        // 在创建线程的时候，直接把 Runnable 当作参数传入，并在其中实现 run 方法
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i ++) {
+                    System.out.printf("%d ",i);
+                }
+            }
+        });
+
+        // 启动线程
+        thread.start();
+    }
 }
