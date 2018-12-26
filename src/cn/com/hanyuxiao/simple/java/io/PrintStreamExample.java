bb4fc711e79f76be842cda69557464ea3d91f3a9
hanyuxiao <hanyuxiao@baidu.com>
2018-12-26 11:23:18 +0800

    feat：System.out.printf() 的一些使用方法

---
+++ src/cn/com/hanyuxiao/simple/java/io/PrintStreamExample.java
@@ -0,0 +1,25 @@
+package cn.com.hanyuxiao.simple.java.io;
+
+/**
+ * 这里主要是非常常见的 System.out.println() 的一些用法的记录。这里的方法可能命名非常不规范，但是
+ * 确实非常容易被查找到。
+ *
+ * @author hanyuxiao
+ * @since 1.0
+ */
+public class PrintStreamExample {
+
+    public static void main(String[] args) {
+        PrintStreamExample example = new PrintStreamExample();
+        example.printf_s();
+    }
+
+    /**
+     * 主要是在 System.out.printf() 当中使用 %s 的一些技巧
+     */
+    public void printf_s() {
+        String name = "world";
+        // 输出 hello world
+        System.out.printf("hello %s", name);
+    }
+}
