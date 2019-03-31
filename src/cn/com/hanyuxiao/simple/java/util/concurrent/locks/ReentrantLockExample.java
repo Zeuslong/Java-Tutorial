fb069c866ec68cf99f8b7bfc532bb37b361f486e
hanyuxiao <hanorz@foxmail.com>
2019-03-31 23:11:32 +0800

    feat ReentrantLock 基本使用

---
+++ src/cn/com/hanyuxiao/simple/java/util/concurrent/locks/ReentrantLockExample.java
@@ -0,0 +1,33 @@
+package cn.com.hanyuxiao.simple.java.util.concurrent.locks;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * {@link java.util.concurrent.locks.ReentrantLock} 的使用示例。
+ */
+public class ReentrantLockExample {
+
+    /**
+     * 这是标准的锁申明的方法。
+     */
+    private final ReentrantLock lock = new ReentrantLock();
+
+    public static void main(String[] args) {
+        ReentrantLockExample reentrantLockExample = new ReentrantLockExample();
+        reentrantLockExample.lock();
+    }
+
+    /**
+     * 锁的一个非常基本的用法，也是使用的一个最佳实践。
+     */
+    private void lock() {
+        // 上锁
+        lock.lock();
+        try {
+            System.out.println("hello world");
+        } finally {
+            // 释放锁的位置
+            lock.unlock();
+        }
+    }
+}
