439f3a066dae8bbde88c0b09e81375b474ea78f3
hanyuxiao <hanyuxiao@cmiot.chinamobile.com>
2019-07-26 17:19:25 +0800

    feat: 1. printf() 基础操作。

--- src/cn/com/hanyuxiao/java/tutorial/io/PrintStreamExample.java
+++ src/cn/com/hanyuxiao/java/tutorial/io/PrintStreamExample.java
@@ -12,14 +12,46 @@ public class PrintStreamExample {
     public static void main(String[] args) {
         PrintStreamExample example = new PrintStreamExample();
         example.printf_s();
+        example.printf_b();
+        example.printf_d();
+        example.printf_length();
+        example.printf_t();
     }
 
     /**
-     * 主要是在 System.out.printf() 当中使用 %s 的一些技巧
+     * 主要是在 System.out.printf() 当中使用 %s 的一些技巧。
      */
     public void printf_s() {
         String name = "world";
         // 输出 hello world
-        System.out.printf("hello %s", name);
+        System.out.printf("hello %s\n", name);
+    }
+
+    /**
+     * 输出 boolean 类型的符号。
+     */
+    public void printf_b() {
+        System.out.printf("%b\n", true);
+    }
+
+    /**
+     * 输出整数。
+     */
+    public void printf_d() {
+        System.out.printf("%d\n", 100);
+    }
+
+    /**
+     * 输出固定长度为 10 的整数。
+     */
+    public void printf_length() {
+        System.out.printf("%10d\n", 100);
+    }
+
+    /**
+     * 输出一个缩进符。
+     */
+    public void printf_t() {
+        System.out.printf("\thello world\n");
     }
 }
